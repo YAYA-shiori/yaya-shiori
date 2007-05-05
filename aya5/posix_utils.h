e19d51cee99cfc0ff1090a1e8692d41df0b6b4ed
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2007-05-05 16:31:42 +0000

    gccで文字コードエラーになるのを修正(2)

--- aya5/posix_utils.h
+++ aya5/posix_utils.h
@@ -1,5 +1,5 @@
 /* -*- c++ -*-
-   POSIXｴﾄｶｭｲｼ､ﾇｻﾈ､ｦ･譯ｼ･ﾆ･｣･�･ﾆ･｣ﾎ爍｣
+   POSIX環境下で使うユーティリティ類。
 */
 #ifndef _POSIX_UTILS_H_INCLUDED_
 #define _POSIX_UTILS_H_INCLUDED_
@@ -16,7 +16,5 @@ std::wstring widen(const std::string& str);
 std::string narrow(const std::wstring& str);
 void fix_filepath(std::string& str);
 void fix_filepath(std::wstring& str);
-
-typedef unsigned int UINT;
 
 #endif
