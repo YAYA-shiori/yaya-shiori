7246d0c152339240b0abb69f3572a02aa750829d
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2007-05-20 12:31:59 +0000

    変更忘れ！

--- aya5/make_aya.bat
+++ aya5/make_aya.bat
@@ -1,12 +1,6 @@
 del *.bak
 mkdir tmp\
 
-del /F /S /Q tmp\yaya-src\*
-mkdir tmp\yaya-src\
-svn export --force . tmp\yaya-src
-del /F /S /Q tmp\yaya-src\OriginalAya\*
-rmdir tmp\yaya-src\OriginalAya
-
 del /F /S /Q tmp\yaya\*
 
 mkdir tmp\yaya\
@@ -15,14 +9,9 @@ copy /B /Y .\readme.txt tmp\yaya\readme.txt
 copy /B /Y .\readme-original.txt tmp\yaya\readme-original.txt
 upx --best tmp\yaya\yaya.dll
 
-del /F /S /Q tmp\yaya-src.zip
 del /F /S /Q tmp\yaya.zip
 
-chdir tmp\yaya-src\
-zip -r -9 -q ..\yaya-src.zip *
 chdir ..\yaya\
 zip -r -9 -q ..\yaya.zip *
 chdir ..\..
-
-del /F /S /Q tmp\yaya-src\*
 
