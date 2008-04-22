3cbe2a339814076bd71c24883612087b7cf00022
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2008-04-22 20:38:58 +0000

    iologをoffにしてもログを完全に無効にできない問題修正

--- trunk/aya5/log.cpp
+++ trunk/aya5/log.cpp
@@ -43,6 +43,11 @@ void	CLog::Start(const yaya::string_t &p, int cs, int ml, HWND hw, char il)
 	charset = cs;
 	msglang = ml;
 	iolog   = il;
+
+	if ( ! il ) {
+		enable = 0;
+		return;
+	}
 	
 #if defined(WIN32)
 	// もしhWndがNULLなら起動中のチェックツールを探して取得する
