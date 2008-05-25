6a206e813023c2f18da7ce722480adb97c838898
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2008-05-25 12:44:17 +0000

    Revert rev.139

--- branches/500/manifest.cpp
+++ branches/500/manifest.cpp
@@ -8,7 +8,7 @@
 #include "manifest.h"
 
 const yaya::char_t *aya_name    = L"YAYA";
-const yaya::char_t *aya_version = L"Tc530-2";
+const yaya::char_t *aya_version = L"Tc530-3";
 const yaya::char_t *aya_author  = L"umeici/The Maintenance Shop";
 
 
