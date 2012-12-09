c4530476abcbe2b3b91c42c2cf79719e43de0690
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2012-12-09 13:22:30 +0000

    ASEARCH要素1コでエラーを吐かないように （regex_traitsを更新：ローカル）

--- aya5/sysfunc.cpp
+++ aya5/sysfunc.cpp
@@ -5076,8 +5076,11 @@ CValue	CSystemFunction::ASEARCH(const CValue &arg, yaya::string_t &d, int &l)
 	int	sz = arg.array_size();
 
 	if (sz < 2) {
-		vm.logger().Error(E_W, 8, L"ASEARCH", d, l);
-		SetError(8);
+		//要素1コ＝空配列の探索である。正常。
+		if (sz < 1) {
+			vm.logger().Error(E_W, 8, L"ASEARCHEX", d, l);
+			SetError(8);
+		}
 		return CValue(-1);
 	}
 
@@ -5096,8 +5099,11 @@ CValue	CSystemFunction::ASEARCHEX(const CValue &arg, yaya::string_t &d, int &l)
 	int	sz = arg.array_size();
 
 	if (sz < 2) {
-		vm.logger().Error(E_W, 8, L"ASEARCHEX", d, l);
-		SetError(8);
+		//要素1コ＝空配列の探索である。正常。
+		if (sz < 1) {
+			vm.logger().Error(E_W, 8, L"ASEARCHEX", d, l);
+			SetError(8);
+		}
 		return CValue(F_TAG_ARRAY, 0/*dmy*/);
 	}
 
