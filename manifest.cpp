0d5101878c5bef2a8cfbbdb582955b82b3347777
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2013-01-08 22:15:27 +0000

    STRFORM改良

--- manifest.cpp
+++ manifest.cpp
@@ -8,7 +8,7 @@
 #include "manifest.h"
 
 const yaya::char_t *aya_name    = L"YAYA";
-const yaya::char_t *aya_version = L"Tc543-3";
+const yaya::char_t *aya_version = L"Tc544-1";
 const yaya::char_t *aya_author  = L"umeici/The Maintenance Shop";
 
 
