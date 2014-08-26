68c11a27584ec0c58e15f3adf18ab86fcb275692
ponapalt <ponapalt@5bfa47c6-0f2a-0410-85c2-71542635dde2>
2014-08-26 01:47:31 +0000

    request_debug削除 (関数位置検索処理の改善のみ残した)

--- branches/500/aya5.h
+++ branches/500/aya5.h
@@ -34,12 +34,10 @@ namespace yaya {
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB load(yaya::global_t h, long len);
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB unload();
 extern "C" DLLEXPORT yaya::global_t FUNCATTRIB request(yaya::global_t h, long *len);
-extern "C" DLLEXPORT yaya::global_t FUNCATTRIB request_debug(yaya::global_t h, long *len);
 
 extern "C" DLLEXPORT long FUNCATTRIB multi_load(yaya::global_t h, long len);
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB multi_unload(long id);
 extern "C" DLLEXPORT yaya::global_t FUNCATTRIB multi_request(long id,yaya::global_t h, long *len);
-extern "C" DLLEXPORT yaya::global_t FUNCATTRIB multi_request_debug(long id,yaya::global_t h, long *len);
 
 #if defined(WIN32)
 extern "C" DLLEXPORT BOOL_TYPE FUNCATTRIB logsend(long hwnd);
