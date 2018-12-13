6d23f2f007d8014190b1eb6a0f7be83867b026bb
C.Ponapalt <ponapalt@shillest.net>
2018-12-13 20:27:22 +0900

    システム辞書は別プロジェクトにしたので削除

--- make_aya.bat
+++ make_aya.bat
@@ -8,7 +8,7 @@ copy /B /Y .\Release\yaya.dll tmp\yaya\yaya.dll
 rem copy /B /Y .\Release_EXE\yaya.exe tmp\yaya\yaya.exe
 copy /B /Y .\readme.txt tmp\yaya\readme.txt
 copy /B /Y .\readme-original.txt tmp\yaya\readme-original.txt
-upx --best tmp\yaya\yaya.dll
+rem upx --best tmp\yaya\yaya.dll
 rem upx --best tmp\yaya\yaya.exe
 
 del /F /S /Q tmp\yaya.zip
