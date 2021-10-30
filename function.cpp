c5bd26c5957b43c73f904e8a07911a9e71fa0235
ponapalt <ponapalt@shillest.net>
2021-10-31 04:22:59 +0900

    hash implement v1

--- function.cpp
+++ function.cpp
@@ -491,12 +491,17 @@ void	CFunction::Foreach(CLocalVariable &lvar, CSelecter &output,size_t line,int 
 	else if (value.IsArray()) {
 		sz = value.array_size();
 	}
+    else if (value.IsHash()) {
+        sz = value.hash_size();
+	}
 	else {
 		sz = -1;
 	}
 
 	CValue	t_value;
 	int type;
+	int fromtype = value.GetType();
+	CValueHash::const_iterator hash_iterator = value.hash().begin();
 	
 	for(int foreachcount = 0; foreachcount < sz; ++foreachcount ) {
 		// 代入する要素値を取得
@@ -504,7 +509,15 @@ void	CFunction::Foreach(CLocalVariable &lvar, CSelecter &output,size_t line,int 
 			t_value = s_array[foreachcount];
 		}
 		else {// F_TAG_ARRAY
-			t_value = value.array()[foreachcount];
+			if ( fromtype == F_TAG_ARRAY ) {
+				t_value = value.array()[foreachcount];
+			}
+			else if ( fromtype == F_TAG_HASH ) {
+				t_value.SetType(F_TAG_ARRAY);
+				t_value.array().push_back(hash_iterator->first);
+				t_value.array().push_back(hash_iterator->second);
+				hash_iterator++;
+			}
 		}
 
 		// 代入
@@ -1144,7 +1157,19 @@ char	CFunction::SubstToArray(CCell &vcell, CCell &ocell, CValue &answer, CStatem
 	CValue	value = GetValueRefForCalc(vcell, st, lvar);
 
 	// 更新
-	value.SetArrayValue(t_order, answer);
+    if (value.GetType() == F_TAG_HASH) {
+        if (answer.GetType() == F_TAG_HASH) {
+            if (answer.hash().empty()) {
+                value.hash().erase(t_order.array()[0]);
+            }
+        }
+        else {
+            value.hash()[CValueSub(t_order.array()[0])] = CValueSub(answer);
+        }
+    }
+    else {
+	    value.SetArrayValue(t_order, answer);
+    }
 
 	// 代入
 	switch(vcell.value_GetType()) {
--- value.cpp
+++ value.cpp
@@ -58,6 +58,8 @@ yaya::int_t	CValue::GetValueInt(void) const
 		return yaya::ws_atoll(s_value, 10);
 	case F_TAG_ARRAY:
 		return 0;
+    case F_TAG_HASH:
+        return 0;
 	default:
 		return 0;
 	};
@@ -80,6 +82,8 @@ double	CValue::GetValueDouble(void) const
 	case F_TAG_STRING:
 		return yaya::ws_atof(s_value);
 	case F_TAG_ARRAY:
+		return 0.0;
+	case F_TAG_HASH:
 		return 0.0;
 	default:
 		return 0.0;
@@ -111,6 +115,18 @@ yaya::string_t	CValue::GetValueString(void) const
 				if (it != array().begin())
 					result += VAR_DELIMITER;
 				result += it->GetValueString();
+			}
+			return result;
+		}
+	case F_TAG_HASH: {
+			yaya::string_t	result;
+			for(CValueHash::const_iterator it = hash().begin();
+				it != hash().end(); it++) {
+				if (it != hash().begin())
+					result += VAR_DELIMITER;
+				result += it->first.GetValueString();
+				result += L"=";
+				result += it->second.GetValueString();
 			}
 			return result;
 		}
@@ -150,6 +166,29 @@ yaya::string_t	CValue::GetValueStringForLogging(void) const
 					result += VAR_DELIMITER;
 				tmpstr = it->GetValueString();
 				if (it->GetType() == F_TAG_STRING)
+					AddDoubleQuote(tmpstr);
+				result += tmpstr;
+			}
+			return result;
+		}
+	case F_TAG_HASH: {
+			yaya::string_t	result;
+			yaya::string_t	tmpstr;
+
+			for(CValueHash::const_iterator it = hash().begin();
+				it != hash().end(); it++) {
+				if (it != hash().begin())
+					result += VAR_DELIMITER;
+
+				tmpstr = it->first.GetValueString();
+				if (it->first.GetType() == F_TAG_STRING)
+					AddDoubleQuote(tmpstr);
+				result += tmpstr;
+
+				result += L"=";
+
+				tmpstr = it->second.GetValueString();
+				if (it->second.GetType() == F_TAG_STRING)
 					AddDoubleQuote(tmpstr);
 				result += tmpstr;
 			}
@@ -491,6 +530,18 @@ CValue &CValue::operator =(const CValueArray &value) LVALUE_MODIFIER
 	return *this;
 }
 
+/* -----------------------------------------------------------------------
+ *  operator = (CValueHash)
+ * -----------------------------------------------------------------------
+ */
+CValue &CValue::operator =(const CValueHash &value) LVALUE_MODIFIER
+{
+	type    = F_TAG_HASH;
+	hash() = value;
+
+	return *this;
+}
+
 void CValue::SubstToArray(CValueArray &value) LVALUE_MODIFIER
 {
 	type    = F_TAG_ARRAY;
@@ -550,6 +601,8 @@ int CValue::CalcEscalationTypeNum(const int rhs) const
 		return F_TAG_DOUBLE;
 	case F_TAG_ARRAY:
 		return F_TAG_ARRAY;
+    case F_TAG_HASH:
+        return F_TAG_HASH;
 	}
 	return F_TAG_VOID;
 }
@@ -1006,6 +1059,19 @@ CValue CValue::operator [](const CValue &value) const
 			}
 		}
 	}
+    else if (type == F_TAG_HASH)
+    {
+        if (!aoflg)
+        {
+            if (hash().count(value.array()[0]))
+            {
+                return CValue(hash().find(value.array()[0])->second);
+            }
+            else {
+                return CValue(L"");
+            }
+        }
+    }
 
 	return yaya::string_t();
 }
