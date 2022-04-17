67bba76bbfb984f5b49e92d1899689c751457c1c
C.Ponapalt <ponapalt@shillest.net>
2022-04-17 10:15:40 +0900

    Tc569-9 skip .bak and .tmp

--- manifest.cpp
+++ manifest.cpp
@@ -8,7 +8,7 @@
 #include "manifest.h"
 
 const yaya::char_t *aya_name    = L"YAYA";
-const yaya::char_t *aya_version = L"Tc569-8";
+const yaya::char_t *aya_version = L"Tc569-9";
 const yaya::char_t *aya_author  = L"umeici/The Maintenance Shop";
 
 
