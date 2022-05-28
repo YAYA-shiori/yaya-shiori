2dce48c291b6f5923f25c918ed8d0f672621aeff
C.Ponapalt <ponapalt@shillest.net>
2022-05-29 08:44:37 +0900

    Tc570-1

--- make_aya.bat
+++ make_aya.bat
@@ -4,13 +4,13 @@ mkdir tmp\
 del /F /S /Q tmp\yaya\*
 
 mkdir tmp\yaya\
-mkdir tmp\yaya\messagetxt\
+rem mkdir tmp\yaya\messagetxt\
 copy /B /Y .\Release\yaya.dll tmp\yaya\yaya.dll
 copy /B /Y .\readme.txt tmp\yaya\readme.txt
 copy /B /Y .\readme-original.txt tmp\yaya\readme-original.txt
-copy /B /Y .\messagetxt\*.* tmp\yaya\messagetxt\
+rem copy /B /Y .\messagetxt\*.* tmp\yaya\messagetxt\
 del /F /S /Q tmp\yaya\*.bak
-del /F /S /Q tmp\yaya\messagetxt\*.bak
+rem del /F /S /Q tmp\yaya\messagetxt\*.bak
 
 del /F /S /Q tmp\yaya.zip
 
