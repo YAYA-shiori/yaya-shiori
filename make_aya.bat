45cd31abfbd9845b3374e5d160e1b2a328cc9f4d
C.Ponapalt <ponapalt@shillest.net>
2022-07-10 18:29:01 +0900

    language file separation toautoex/cvautoex

--- make_aya.bat
+++ make_aya.bat
@@ -18,3 +18,21 @@ chdir tmp\yaya\
 zip -r -9 -q ..\yaya.zip *
 chdir ..\..
 
+
+del /F /S /Q tmp\yaya_lang_sep\*
+
+mkdir tmp\yaya_lang_sep\
+mkdir tmp\yaya_lang_sep\messagetxt\
+copy /B /Y .\ReleaseLangSep\yaya.dll tmp\yaya_lang_sep\yaya.dll
+copy /B /Y .\readme.txt tmp\yaya_lang_sep\readme.txt
+copy /B /Y .\readme-original.txt tmp\yaya_lang_sep\readme-original.txt
+copy /B /Y .\messagetxt\*.* tmp\yaya_lang_sep\messagetxt\
+del /F /S /Q tmp\yaya_lang_sep\*.bak
+del /F /S /Q tmp\yaya_lang_sep\messagetxt\*.bak
+
+del /F /S /Q tmp\yaya_lang_sep.zip
+
+chdir tmp\yaya_lang_sep\
+zip -r -9 -q ..\yaya_lang_sep.zip *
+chdir ..\..
+
